336594fa2e2d4ea7e2b515b324d3a8e66629445a
Emmanuel Jourdan <ej@antescofo.com>
2026-06-22 23:02:08 +0100

    wrapping everything within a single transaction
    doesn't seem to work

--- src/extension.ts
+++ src/extension.ts
@@ -7,24 +7,28 @@ import {
 export function activate(activation: ActivationContext) {
   const context = initialize(activation, "1.0.0");
 
-  // iterate through every tracks,
-  // find the AudioClips in session and arrangement
-  // disable Warp
-  const tracks = context.application.song.tracks.forEach((track) => {
-    track.clipSlots.forEach((clipSlot) => {
-      const clip = clipSlot.clip;
-      if (clip instanceof AudioClip) {
-        clip.warping = false;
-        console.log(`Disabled warping: ${clip?.name}`);
-      }
-    });
+  context.withinTransaction(() => {
+    // iterate through every tracks,
+    // find the AudioClips in session and arrangement
+    // disable Warp
+    const tracks = context.application.song.tracks.forEach((track) => {
+      // Session's clips
+      track.clipSlots.forEach((clipSlot) => {
+        const clip = clipSlot.clip;
+        if (clip instanceof AudioClip) {
+          clip.warping = false;
+          console.log(`Disabled warping: ${clip?.name}`);
+        }
+      });
 
-    track.arrangementClips.forEach((arrangementClip) => {
-      const clip = arrangementClip;
-      if (clip instanceof AudioClip) {
-        clip.warping = false;
-        console.log(`Disabled warping: ${clip?.name}`);
-      }
+      // Arrangement's Clips
+      track.arrangementClips.forEach((arrangementClip) => {
+        const clip = arrangementClip;
+        if (clip instanceof AudioClip) {
+          clip.warping = false;
+          console.log(`Disabled warping: ${clip?.name}`);
+        }
+      });
     });
   });
 }
